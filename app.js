f9de945d559d86d09dc016b15ad363d7b1bcfe77
giordifungula <giordifungula@gmail.com>
2019-02-07 19:28:23 +0200

    added top page content to app

--- app.js
+++ app.js
@@ -33,5 +33,10 @@ app.use('/view', viewRouter);
 app.use('/update', updateRouter);
 app.use('/delete', deleteRouter);
 
+// app.use('/', express.static(__dirname + '/www')); // redirect root
+// app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
+// app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
+// app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
+
 module.exports = app;
 app.listen(8080);
